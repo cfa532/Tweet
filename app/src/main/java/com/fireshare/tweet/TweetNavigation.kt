ad006dfce62fd86a0f8e8d26e19c75645a79901f
cfa532 <cfa532@yahoo.com>
2024-08-18 10:35:09 +0800

    Application name added

--- app/src/main/java/com/fireshare/tweet/TweetNavigation.kt
+++ app/src/main/java/com/fireshare/tweet/TweetNavigation.kt
@@ -1,2 +1,33 @@
 package com.fireshare.tweet
 
+import androidx.navigation.NavHostController
+import com.fireshare.tweet.TweetDestinationsArgs.TWEET_ID_ARGS
+import com.fireshare.tweet.TweetScreens.FEED_SCREEN
+import com.fireshare.tweet.TweetScreens.TWEET_SCREEN
+
+private object TweetScreens {
+    const val FEED_SCREEN = "tweetFeed"
+    const val TWEET_SCREEN = "tweetDetail"
+    const val PROFILE_SCREEN = "userProfile"
+    const val EDIT_SCREEN = "tweetEditor"
+    const val MESSAGE_SCREEN = "userMessages"
+}
+
+object TweetDestinationsArgs {
+    const val TWEET_ID_ARGS = "tweetId"
+}
+
+object TweetDestinations {
+    const val TWEET_ROUTE = "$TWEET_SCREEN?$TWEET_ID_ARGS={$TWEET_ID_ARGS}"
+}
+
+class TweetNavigationActions(private val navController: NavHostController) {
+
+    fun navigateToFeed() {
+        navController.navigate(FEED_SCREEN)
+    }
+
+    fun navigateToTweet() {
+
+    }
+}
