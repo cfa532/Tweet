e3e332598ba2a5c4b7430c22749e58ddda6c3b88
cfa532 <cfa532@yahoo.com>
2024-08-24 08:26:37 +0800

    different viewModel returned.

--- app/src/main/java/com/fireshare/tweet/tweet/TweetItem.kt
+++ app/src/main/java/com/fireshare/tweet/tweet/TweetItem.kt
@@ -9,8 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.fireshare.tweet.TweetViewModelFactory
 import com.fireshare.tweet.datamodel.Tweet
 import com.fireshare.tweet.viewmodel.TweetViewModel
 
