6281552aeaa0a2c6d11afcf0a515ec1e2b7d4759
cfa532 <cfa532@yahoo.com>
2024-09-25 23:52:22 +0800

    Full video view added OK

--- app/src/main/java/com/fireshare/tweet/widget/MediaBrowser.kt
+++ app/src/main/java/com/fireshare/tweet/widget/MediaBrowser.kt
@@ -17,8 +17,10 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,6 +28,11 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
+import com.fireshare.tweet.widget.Gadget.detectMimeTypeFromHeader
+import com.fireshare.tweet.widget.Gadget.downloadFileHeader
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun MediaBrowser(navController: NavController, mediaItems: List<MediaItem>, startIndex: Int) {
@@ -55,11 +62,32 @@ fun MediaBrowser(navController: NavController, mediaItems: List<MediaItem>, star
                 modifier = Modifier.fillMaxSize()
             ) { page ->
                 val mediaItem = mediaItems[page]
-                if (mediaItem.type == MediaType.Video) {
-                    // Directly play the video without caching
-                    VideoPlayer(uri = Uri.parse(mediaItem.url))
-                } else {
-                    ImageViewer(mediaItem.url, isPreview = false)
+                if (mediaItem.type == MediaType.Unknown) {
+                    val coroutineScope = rememberCoroutineScope()
+                    LaunchedEffect(mediaItem.url.substringAfterLast("/")) {
+                        coroutineScope.launch {
+                            val header = withContext(Dispatchers.IO) {
+                                downloadFileHeader(mediaItem.url)
+                            }
+                            detectMimeTypeFromHeader(header)?.let {
+                                mediaItem.type = when (it.substringBefore("/")) {
+                                    "image" -> MediaType.Image
+                                    "audio" -> MediaType.Audio
+                                    "video" -> MediaType.Video
+                                    else -> MediaType.Unknown
+                                }
+                            }
+                        }
+                    }
+                }
+                when (mediaItem.type) {
+                    MediaType.Video, MediaType.Audio -> {
+                        // Consider caching for larger videos
+                        VideoPlayer(uri = Uri.parse(mediaItem.url))
+                    }
+                    else -> {
+                        ImageViewer(mediaItem.url, isPreview = false)
+                    }
                 }
             }
         }
