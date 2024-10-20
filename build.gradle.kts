42242b77e3b7ae1a2b14329a4838da74f7bd7260
cfa532 <cfa532@yahoo.com>
2024-10-20 21:51:29 +0800

    Kapt to Ksp ok

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.52" apply false
     alias(libs.plugins.compose.compiler) apply false
     kotlin("jvm") version "2.0.21"
-    kotlin("plugin.serialization") version "2.0.20" apply false
+    kotlin("plugin.serialization") version "2.0.21" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.25"
 }
