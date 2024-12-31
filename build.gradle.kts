8a3d66cced0070e551cb18d192abd30149b37226
cfa532 <cfa532@yahoo.com>
2024-12-31 10:30:20 +0800

    Kotlin 2.1.0 upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
+    id("com.google.dagger.hilt.android") version "2.54" apply false
     alias(libs.plugins.compose.compiler) apply false
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.serialization") version "2.0.21" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.25"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.28"
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
