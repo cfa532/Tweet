4c4f3c8ac09c2d85012ed14e3cd463287cd29630
cfa532 <cfa532@yahoo.com>
2025-03-16 10:31:43 +0800

    UserAvatar update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,12 +40,11 @@ android {
             ndk {
                 debugSymbolLevel = "FULL"
             }
-            buildConfigField("String", "BASE_URL", "\"tweet.fireshare.uk\"")
-            buildConfigField("String", "APP_ID", "\"heWgeGkeBX2gaENbIBS_Iy1mdTS\"")
-            // Sync APP_ID_HASH to the same nodes of APP_ID manually for it to work.
+            buildConfigField("String", "BASE_URL", "\"twbe.fireshare.uk\"")
+            buildConfigField("String", "APP_ID", "\"d4lRyhABgqOnqY4bURSm_T-4FZ4\"")
             buildConfigField("String", "APP_ID_HASH", "\"FGPaNfKA-RwvJ-_hGN0JDWMbm9R\"")
-            buildConfigField("String", "ALPHA_ID", "\"mwmQCHCEHClCIJy-bItx5ALAhq9\"")
-            buildConfigField("String", "ENTRY_URLS", "\"dSXMdZNrpMw0xJQEbxPZn5nnLBK\"")
+            buildConfigField("String", "ALPHA_ID", "\"iFG4GC9r0fF22jYBCkuPThybzwO\"")
+            buildConfigField("String", "ENTRY_URLS", "\"1x7Dh9mJfN5zSyPM5TRX3Sro_wQna\"")
         }
         release {
             isMinifyEnabled = true
