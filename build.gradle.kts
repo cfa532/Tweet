1dcc095867c313976a12821560294cfff60380ca
cfa532 <cfa532@yahoo.com>
2025-05-20 23:36:02 +0800

    ksp kotlin version solved

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,6 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.28"
+    id("com.google.devtools.ksp") version "2.1.21-2.0.1"
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
