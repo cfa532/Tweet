ba37b66fe1192f4ce76578915820203b86fc1b9a
cfa532 <cfa532@yahoo.com>
2026-09-21 18:25:21 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@
 plugins {
     alias(libs.plugins.android.application) apply false
 
-    id("com.google.dagger.hilt.android") version "2.59.2" apply false
+    id("com.google.dagger.hilt.android") version "2.60.1" apply false
     alias(libs.plugins.compose.compiler) apply false
     kotlin("jvm") version "2.4.0" apply false
     kotlin("plugin.serialization") version "2.4.0" apply false
     alias(libs.plugins.ksp) apply false
     id("com.google.gms.google-services") version "4.5.0" apply false
-    id("androidx.room") version "2.8.4" apply false
+    id("androidx.room") version "2.8.5" apply false
 }
